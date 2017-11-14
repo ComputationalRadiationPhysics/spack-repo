0e10972db60fa5a8fc7dbcf9e5b2a81c73570a5f
Axel Huebl <axel.huebl@plasma.ninja>
2017-11-14 16:26:10 +0100

    PIConGPU: CMake is "run" as well
    since we need it at "runtime" to build via our scripts

--- packages/picongpu/package.py
+++ packages/picongpu/package.py
@@ -64,7 +64,7 @@ class Picongpu(Package):
 
     # @TODO add type=('link, 'run') to all these?
     # @TODO define supported ranges instead of fixed versions
-    depends_on('cmake@3.7:3.9', type='build')
+    depends_on('cmake@3.7:3.9', type=['build', 'run'])
     depends_on('cuda@7.5:9', when='backend=cuda')
     depends_on('zlib@1.2.11')
     depends_on('boost@1.62.0:')
