95e3c48a1212035492c5bcfad4e5a65cc0fe38b4
Axel Huebl <axel.huebl@plasma.ninja>
2017-11-14 16:56:02 +0100

    PIConGPU: Fix SPACK_ROOT

--- packages/picongpu/package.py
+++ packages/picongpu/package.py
@@ -121,7 +121,7 @@ class Picongpu(Package):
         profile_out = join_path(path_etc, 'picongpu')
         install(profile_in, profile_out)
         filter_file('@PIC_SPACK_COMPILER@', str(self.compiler.spec),
-        filter_file('@PIC_SPACK_ROOT@', str(self.rootdir),
+        filter_file('@PIC_SPACK_ROOT@', str(os.environ['SPACK_ROOT']),
                     join_path(profile_out, 'picongpu.profile'))
         #filter_file('@PIC_SPACK_COMPILER@', str(self.compiler.spec),
         #            join_path(profile_out, 'picongpu.profile'))
