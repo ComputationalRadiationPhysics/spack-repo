7557c711d9fd79ced8b72cd82c4cb801dfb133a2
Axel Huebl <axel.huebl@plasma.ninja>
2019-02-23 15:30:18 +0100

    PIConGPU 0.4.3: Boost 1.66.0 ok
    With CUDA 9.0, Boost 1.66.0 should still work:
    
      https://github.com/ComputationalRadiationPhysics/picongpu/pull/2908

--- packages/picongpu/package.py
+++ packages/picongpu/package.py
@@ -89,8 +89,8 @@ class Picongpu(Package):
     depends_on('util-linux', type='run', when='platform=darwin')  # GNU getopt
     depends_on('cuda@8.0:9.2', when='backend=cuda')
     depends_on('zlib@1.2.11')
-    depends_on('boost@1.62.0:1.65.1 cxxstd=11')
-    depends_on('boost@1.65.1 cxxstd=11', when='backend=cuda ^cuda@9:')
+    depends_on('boost@1.62.0:1.66.0 cxxstd=11')
+    depends_on('boost@1.65.1:1.66.0 cxxstd=11', when='backend=cuda ^cuda@9:')
     # note: NOT cuda aware!
     # depends_on('mpi@2.3:', type=['link', 'run'])
     depends_on('openmpi@1.8:3.99', type=['link', 'run'])
